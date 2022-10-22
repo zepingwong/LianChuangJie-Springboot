1334f74a362ec0acff73bf6b5c6a3be45a7e596c
WANG Zeping <zepingwong@gmail.com>
2022-10-23 00:58:27 +0800

    password

--- src/main/java/com/lianchuangjie/lianchuangjie/service/User/impl/UserPasswordServiceImpl.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/service/User/impl/UserPasswordServiceImpl.java
@@ -3,6 +3,7 @@ package com.lianchuangjie.lianchuangjie.service.User.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lianchuangjie.lianchuangjie.dto.UserPasswordDTO;
 import com.lianchuangjie.lianchuangjie.entity.UserEntity;
+import com.lianchuangjie.lianchuangjie.exception.ResponseEnum;
 import com.lianchuangjie.lianchuangjie.mapper.UserMapper;
 import com.lianchuangjie.lianchuangjie.service.User.UserPasswordService;
 import com.lianchuangjie.lianchuangjie.utils.SecurityUtil;
@@ -17,7 +18,12 @@ public class UserPasswordServiceImpl extends ServiceImpl<UserMapper, UserEntity>
     UserMapper userMapper;
     @Override
     public Boolean setUserPassword(UserPasswordDTO userPasswordDTO) {
-        UserEntity user = SessionUtil.getUser();
+        UserEntity user = userMapper.selectById(SessionUtil.getUser().getUserSign());
+        Boolean match = SecurityUtil.matchesPassword(userPasswordDTO.getOldPassword(), user.getUPasswordWeb());
+        ResponseEnum.PASSWORD_ERROR.assertIsFalse(match, "原密码错误"); // 原密码错误
+        ResponseEnum.FAILURE.assertNotEqual(userPasswordDTO.getConfirmPassword(), userPasswordDTO.getNewPassword()); // 两次密码不一致
+        match = SecurityUtil.matchesPassword(userPasswordDTO.getNewPassword(), user.getUPasswordWeb());
+        ResponseEnum.PASSWORD_ERROR.assertIsTrue(match, "密码未修改"); // 密码未修改
         user.setUPasswordWeb(SecurityUtil.encodePassword(userPasswordDTO.getNewPassword()));
         return userMapper.updateById(user) == 1;
     }
