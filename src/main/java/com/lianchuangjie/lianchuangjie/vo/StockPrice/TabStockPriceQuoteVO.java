77efa7c229c0baedafbc44f8838d25b15871742a
WANG Zeping <zepingwong@gail.com>
2022-10-27 21:16:59 +0800

    tab

--- src/main/java/com/lianchuangjie/lianchuangjie/vo/StockPrice/TabStockPriceQuoteVO.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/vo/StockPrice/TabStockPriceQuoteVO.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -16,7 +15,6 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Accessors(chain = true)
 public class TabStockPriceQuoteVO {
     // 报价日期
     @JsonProperty("U_QuoDate")
