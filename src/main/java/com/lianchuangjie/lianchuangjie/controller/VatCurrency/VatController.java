e357c7966e91efcd9ad179a1558a615826855f4e
WANG Zeping <zepingwong@gmail.com>
2022-10-28 14:59:21 +0800

    vat group

---
+++ src/main/java/com/lianchuangjie/lianchuangjie/controller/VatCurrency/VatController.java
@@ -0,0 +1,22 @@
+package com.lianchuangjie.lianchuangjie.controller.VatCurrency;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
+import com.lianchuangjie.lianchuangjie.config.Authentication;
+import com.lianchuangjie.lianchuangjie.entity.VatCurConfEntity;
+import com.lianchuangjie.lianchuangjie.entity.VatGroupEntity;
+import com.lianchuangjie.lianchuangjie.service.VatCurConfService;
+import com.lianchuangjie.lianchuangjie.service.VatGroupService;
+import com.lianchuangjie.lianchuangjie.utils.Result;
+import com.lianchuangjie.lianchuangjie.vo.VatCurConfVO;
+import com.lianchuangjie.lianchuangjie.vo.VatGroupVO;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/vat")
+public class VatController {
+
+}
--- src/main/java/com/lianchuangjie/lianchuangjie/controller/VatCurrency/VatCurController.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/controller/VatCurrency/VatCurController.java
@@ -1,4 +1,4 @@
-package com.lianchuangjie.lianchuangjie.controller;
+package com.lianchuangjie.lianchuangjie.controller.VatCurrency;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
@@ -6,10 +6,8 @@ import com.lianchuangjie.lianchuangjie.config.Authentication;
 import com.lianchuangjie.lianchuangjie.entity.VatCurConfEntity;
 import com.lianchuangjie.lianchuangjie.entity.VatGroupEntity;
 import com.lianchuangjie.lianchuangjie.service.VatCurConfService;
-import com.lianchuangjie.lianchuangjie.service.VatGroupService;
 import com.lianchuangjie.lianchuangjie.utils.Result;
 import com.lianchuangjie.lianchuangjie.vo.VatCurConfVO;
-import com.lianchuangjie.lianchuangjie.vo.VatGroupVO;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -17,45 +15,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/vat")
-public class VatController {
-    @Resource
-    VatGroupService vatGroupService;
+public class VatCurController {
     @Resource
     VatCurConfService vatCurConfService;
-
-    /**
-     * @param Type 税率类型
-     * @return List<VatGroupVO>
-     * @description 获取 VatGroup 列表
-     */
-    @GetMapping("/group")
-    @Authentication(buyer = true, sale = true)
-    public Result<List<VatGroupVO>> getVatGroupListController(
-            @RequestParam(defaultValue = "#{null}", value = "Type") String Type
-    ) {
-        QueryWrapper<VatGroupVO> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("Type", Type);
-        List<VatGroupVO> list = vatGroupService.list(queryWrapper);
-        return Result.success(list, "Success");
-    }
-
-    @GetMapping("/group/{code}")
-    public Result<VatGroupVO> getVatGroupMainController(
-            @PathVariable String code
-    ) {
-        QueryWrapper<VatGroupVO> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("Code", code);
-        VatGroupVO vatGroup = vatGroupService.getOne(queryWrapper);
-        return Result.success(vatGroup, "Success");
-    }
-
     @GetMapping("/config")
     @Authentication(buyer = true, sale = true)
     public Result<List<VatCurConfVO>> getVatConfigListController(
-            @RequestParam(defaultValue = "#{null}", value = "Type") String Type
+            @RequestParam(defaultValue = "#{null}", value = "Type") String type
     ) {
         QueryWrapper<VatCurConfVO> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq(SqlHelper.table(VatGroupEntity.class).getTableName() + ".Type", Type);
+        if (type != null) {
+            queryWrapper.eq(SqlHelper.table(VatGroupEntity.class).getTableName() + ".Type", type);
+        }
         List<VatCurConfVO> list = vatCurConfService.list(queryWrapper);
         return Result.success(list, "Success");
     }
