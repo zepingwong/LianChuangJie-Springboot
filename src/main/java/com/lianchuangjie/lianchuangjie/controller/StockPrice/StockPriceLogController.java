fdfc50b441e2a649a8985936342717b19fb205ff
WANG Zeping <zepingwong@gmail.com>
2022-11-04 20:19:31 +0800

    search

--- src/main/java/com/lianchuangjie/lianchuangjie/controller/StockPrice/StockPriceLogController.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/controller/StockPrice/StockPriceLogController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 @RestController
 @Validated
@@ -23,11 +24,19 @@ public class StockPriceLogController extends BaseController {
     @GetMapping("/log")
     public Result<Page<StockPriceLogVO>> getLogList(
             @RequestParam(defaultValue = "#{null}", value = "page") Integer page,
-            @RequestParam(defaultValue = "#{null}", value = "size") Integer size
+            @RequestParam(defaultValue = "#{null}", value = "size") Integer size,
+            @RequestParam(defaultValue = "#{null}", value = "TimeStart") Date timeStart,
+            @RequestParam(defaultValue = "#{null}", value = "TimeEnd") Date timeEnd,
+            @RequestParam(defaultValue = "#{null}", value = "TriggerType") String triggerType,
+            @RequestParam(defaultValue = "#{null}", value = "TriggerName") String triggerName
     ) {
         StockPriceLogSearchDTO stockPriceLogSearchDTO = new StockPriceLogSearchDTO();
         stockPriceLogSearchDTO.setPage(page);
         stockPriceLogSearchDTO.setSize(size);
+        stockPriceLogSearchDTO.setTimeStart(timeStart);
+        stockPriceLogSearchDTO.setTimeEnd(timeEnd);
+        stockPriceLogSearchDTO.setTriggerType(triggerType);
+        stockPriceLogSearchDTO.setTriggerName(triggerName);
         Page<StockPriceLogVO> pages  = stockPriceLogService.list(stockPriceLogSearchDTO);
         return Result.success(pages, "Success");
     }
--- src/main/java/com/lianchuangjie/lianchuangjie/dto/StockPrice/StockPriceLogSearchDTO.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/dto/StockPrice/StockPriceLogSearchDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +14,12 @@ public class StockPriceLogSearchDTO {
     private Integer page;
     // 每页显示数量
     private Integer size;
+    // 开始时间
+    private Date timeStart;
+    // 结束时间
+    private Date timeEnd;
+    // 事件类型
+    private String triggerName;
+    // 触发方式
+    private String triggerType;
 }
