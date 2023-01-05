5408a25b6015d809c809fef6a0c722aaa18a736f
WANGZeping <79504116+zepingwong@users.noreply.github.com>
2023-01-05 14:02:37 +0800

    is sale assist

--- src/main/java/com/lianchuangjie/lianchuangjie/utils/AuthUtil.java
+++ src/main/java/com/lianchuangjie/lianchuangjie/utils/AuthUtil.java
@@ -3,6 +3,7 @@ package com.lianchuangjie.lianchuangjie.utils;
 
 import com.lianchuangjie.lianchuangjie.config.Authentication;
 import com.lianchuangjie.lianchuangjie.entity.User.UserEntity;
+import org.assertj.core.api.Object2DArrayAssert;
 
 import java.util.Objects;
 /**
@@ -12,18 +13,20 @@ import java.util.Objects;
 public class AuthUtil {
     private final Boolean isBuyer;
     private final Boolean isSale;
+    private final Boolean isSaleAssist;
 
     public AuthUtil(Authentication authentication) {
         this.isBuyer = authentication.buyer();
         this.isSale = authentication.sale();
+        this.isSaleAssist = authentication.saleAssist();
     }
     public Boolean checkAuth(UserEntity user) {
         // 管理员可以访问所有接口
         // 其他角色需要鉴权
-        System.out.println(user);
         return (Objects.equals(user.getSuperUser(), "Y") |
                 (this.isBuyer & Objects.equals(user.getUIsBuyer(), "Y")) |
-                (this.isSale & Objects.equals(user.getUIsSale(), "Y"))
+                (this.isSale & Objects.equals(user.getUIsSale(), "Y")) |
+                (this.isSaleAssist & Objects.equals(user.getUIsSaleAssist(), "Y"))
         );
     }
 }
